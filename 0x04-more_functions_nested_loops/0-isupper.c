b32f853623241a93026a140e65336a7949bcb19b
kweyuremote <remotaskreport@gmail.com>
2023-07-21 16:51:42 -0700

    function to check for uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,12 +9,9 @@
 int _isupper(int c)
 
 {
- char c;
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-
-    return (0);
 }
