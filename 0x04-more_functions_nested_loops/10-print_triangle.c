d6bb087774e0157c2e72e7eebf631f3bec05f18c
kweyuremote <remotaskreport@gmail.com>
2023-07-21 18:00:58 -0700

    the triangle printer

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - entry point
+ * Description: Prints diagonals
+ *@size: size of the triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int row,symobol, spaces;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (symbol = 1; symbol <= row; symboly++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
